ed85dc91752d738ee54fad41fb50f62904f8ae9c
Eddy Martin <eddypmartin@gmail.com>
2023-03-08 10:51:30 -0500

    c11 Début du carrousel

---
+++ carrousel.php
@@ -0,0 +1,13 @@
+<?php
+/**
+* Extension carrousel permet d'afficher dans une boîte modale 
+* les images d'une galerie
+* Package name : Carrousel
+* Version: 1.0.0
+*/
+/*
+Plugin name: Carrousel
+Author: Eddy Martin
+Plugin URI: https:github.com/eddytuto/carrousel
+Description: Permet d'afficher dans une boîte modale les images d'une galerie avec un système de navigation
+*/
